4d3b802c839d5e382001a1ed5f394f7dd74c6ea6
apoorvdixit99 <apoorvdixit619@gmail.com>
2019-07-23 13:45:11 +0530

    Initial commit

---
+++ app/src/main/java/in/dareal/locationtracker/GPSTracker.java
@@ -0,0 +1,116 @@
+package in.dareal.locationtracker;
+
+import android.Manifest;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+
+import androidx.core.content.ContextCompat;
+
+public class GPSTracker extends Service implements LocationListener {
+
+    private final Context context;
+
+    boolean isNetworkEnabled = false;
+    boolean isGPSEnabled = false;
+
+    Location location;
+    protected LocationManager locationManager;
+
+    public GPSTracker(Context context){
+        this.context = context;
+    }
+
+    public Location getLocation(){
+        try{
+            locationManager = (LocationManager) context.getSystemService(LOCATION_SERVICE);
+            isGPSEnabled = locationManager.isProviderEnabled(locationManager.GPS_PROVIDER);
+            isNetworkEnabled = locationManager.isProviderEnabled(locationManager.NETWORK_PROVIDER);
+
+            if( ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                    || ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED ){
+
+                if(isGPSEnabled){
+                    if(location==null){
+                        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10000,10,this);
+                        if(locationManager!=null){
+                            location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                        }
+                    }
+                }
+
+                if(location==null){
+                    if(isNetworkEnabled){
+                        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10000,10,this);
+                        if(locationManager!=null){
+                            location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                        }
+                    }
+                }
+
+            }
+        }
+        catch(Exception e){
+
+        }
+        return location;
+    }
+
+
+
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+}
+
+/*
+*
+*     private GPSTracker gpsTracker;
+
+    private Location mLocation;
+    double mLatitude, mLongitude;
+
+
+
+        gpsTracker = new GPSTracker(getApplicationContext());
+        mLocation = gpsTracker.getLocation();
+
+        mLatitude = mLocation.getLatitude();
+        mLongitude = mLocation.getLongitude();
+
+
+                LatLng mycurrentlocation = new LatLng(mLatitude, mLongitude);
+        mMap.addMarker(new MarkerOptions().position(mycurrentlocation)
+                .title("My Current Location"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mycurrentlocation,20f));
+*
+*
+* */
